e7da5a3061d1e562493589991114cd3dab51a77e
SanLLL <88852249+SanLLL@users.noreply.github.com>
2026-07-29 22:12:04 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-const SUPABASE_URL = "https://msnnsnatkozozlrenfrg.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zbm5zbmF0a296b3pscmVuZnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzNDIwOTYsImV4cCI6MjEwMDkxODA5Nn0.BkfWuj8Ce7_9XqlNBEatNpYInZKn0IjAqOkjNUe5Wb4";
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
