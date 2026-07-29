717bac9043b58ec8fb23e0a5fb18090321189da1
SanLLL <88852249+SanLLL@users.noreply.github.com>
2026-07-29 22:03:06 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://msnnsnatkozozlrenfrg.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://msnnsnatkozozlrenfrg.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zbm5zbmF0a296b3pscmVuZnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzNDIwOTYsImV4cCI6MjEwMDkxODA5Nn0.BkfWuj8Ce7_9XqlNBEatNpYInZKn0IjAqOkjNUe5Wb4";
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
