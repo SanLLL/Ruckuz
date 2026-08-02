180e8801e382657100a70cfddd38591e94cddb2d
SanLLL <88852249+SanLLL@users.noreply.github.com>
2026-08-02 21:57:34 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,6 @@
-const supabase = window.supabase.createClient(
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
     "https://msnnsnatkozozlrenfrg.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zbm5zbmF0a296b3pscmVuZnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzNDIwOTYsImV4cCI6MjEwMDkxODA5Nn0.BkfWuj8Ce7_9XqlNBEatNpYInZKn0IjAqOkjNUe5Wb4"
 );
